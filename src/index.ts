6259d751d56e48174109ca683109eb9a730db5ee
Aniket <aniketraj28042003@gmail.com>
2025-04-25 14:52:23 +0530

    initialized project

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { corsOptions } from './util/cors.util';
+import { PORT } from './config/env.config';
+
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).json({ message: 'Server is alive' });
+  return;
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
