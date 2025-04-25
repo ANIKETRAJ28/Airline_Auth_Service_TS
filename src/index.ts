fa94afe5f13e940d80b77458ec67345ace79cd29
Aniket <aniketraj28042003@gmail.com>
2025-04-25 22:07:31 +0530

    add: routes end point to index file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { corsOptions } from './util/cors.util';
 import { PORT } from './config/env.config';
+import { router } from './routes/index.route';
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.get('/', (_req: Request, res: Response) => {
   res.status(200).json({ message: 'Server is alive' });
   return;
 });
+
+app.use('/api/v1', router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
