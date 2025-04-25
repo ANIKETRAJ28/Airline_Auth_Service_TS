0fcadc699853c1774810bb95c986352b3baaacce
Aniket <aniketraj28042003@gmail.com>
2025-04-25 22:02:02 +0530

    update: config: updated env configs

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 5000;
 export const FRONTEND_URL = process.env.FRONTEND_URL;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
@@ -10,3 +10,4 @@ export const DB_PORT = process.env.DB_PORT;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_URL = process.env.DB_URL;
 export const NODE_ENV = process.env.NODE_ENV;
+export const SALT = process.env.SALT || 10;
