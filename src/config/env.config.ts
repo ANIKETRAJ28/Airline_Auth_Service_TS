4646fb8aa5efaf79b6c99b21125f27974da75229
Aniket <aniketraj28042003@gmail.com>
2025-04-25 18:33:16 +0530

    add: config | util: added db connection configs and utils

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,3 +3,10 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const FRONTEND_URL = process.env.FRONTEND_URL;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_NAME = process.env.DB_NAME;
+export const DB_URL = process.env.DB_URL;
+export const NODE_ENV = process.env.NODE_ENV;
