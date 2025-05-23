cf8f15f970fd4220c7098b633a58ee26a583ce8f
Aniket <aniketraj28042003@gmail.com>
2025-05-23 09:42:56 +0530

    add: routes: user.route: added routes for logout and login

--- src/routes/v1_routes/user.route.ts
+++ src/routes/v1_routes/user.route.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { UserController } from '../../controller/user.controller';
+import { jwtMiddleware } from '../../middleware/auth.middleware';
 
 export const userRouter = Router();
 const userController = new UserController();
 
+userRouter.get('/verify', jwtMiddleware);
+userRouter.get('/logout', userController.logout.bind(userController));
 userRouter.get('/id/:id', userController.getUserById.bind(userController));
 userRouter.get('/email/:email', userController.getUserByEmail.bind(userController));
 userRouter.get('/:id/admin', userController.isUserAdmin.bind(userController));
+userRouter.post('/login', userController.loginUser.bind(userController));
 userRouter.post('/', userController.createUser.bind(userController));
